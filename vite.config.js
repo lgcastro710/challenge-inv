7dacf1885fce29d86997d8a27f4b3c1515fd5988
lgcastro710 <gerardocastro710gmail.com>
2023-09-11 19:57:24 -0300

    add url github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/challenge-inv/",
+  base: "http://lgcastro710.github.io/challenge-inv/",
 })
